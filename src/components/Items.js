3007e796064ed3bef51f315a1112327795f35fbe
KohinaTheCat <50504825+KohinaTheCat@users.noreply.github.com>
2020-05-05 21:22:02 -0400

    ui

---
+++ src/components/Items.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../css/styles.css";
+
+export default function Items(props) {
+  if (props.clicked) {
+    return <div className="item clicked"></div>;
+  } else {
+    return <div className="item"></div>;
+  }
+}
